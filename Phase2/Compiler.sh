ce1e115e28774d0626237971ce366b7620d65bd0
Sajede Nicknadaf <59578311+SajedeNick1999@users.noreply.github.com>
2020-08-16 18:25:49 +0430

    Phase1

---
+++ Phase2/Compiler.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+Command=${1}
+PrintMode=${2}
+FileName=${3}
+
+if [ $Command == "--run" ]
+then
+	bison -d Compiler.y
+	echo "Compiler.tab.h is created"
+	echo "Compiler.tab.c is created"
+	flex Compiler.l
+	echo "lex.yy.c is created"
+	g++ lex.yy.c Compiler.tab.c
+	echo "Compiling has finished and execution file is created"
+	./a.out $PrintMode $FileName
+elif [ $Command == "--remove" ]
+then
+	rm Compiler.tab.c Compiler.tab.h lex.yy.c a.out
+	echo "All files removed :)"
+else
+	echo "Unkown command :| "
+fi
